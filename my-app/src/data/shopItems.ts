d944438a5f507f3bd06fe021b61b01c2c6d2994c
Joshua Brazington <braz.josh@yahoo.co.uk>
2026-03-30 10:58:49 +0100

    add images for the shop hardcode the shop items locally

---
+++ my-app/src/data/shopItems.ts
@@ -0,0 +1,19 @@
+import firstAidIcon from '../assets/first-aid-kit-icon.png'
+import fuelIcon from '../assets/fuel-icon.png'
+import rationsIcon from '../assets/rations-icon.png'
+import waterIcon from '../assets/water-icon.png'
+
+export type Item = {
+    id: number
+    name: string
+    image: string
+    price: number
+    stock: number
+}
+
+export const items = [
+    { id: 1, name: 'Water Crate', image: waterIcon, price: 9.99, stock: 200},
+    { id: 2, name: 'Rations Box', image: rationsIcon, price: 15.99, stock: 200 },
+    { id: 3, name: 'First Aid Kit', image: firstAidIcon, price: 10.99, stock: 10  },
+    { id: 4, name: 'Fuel Can', image: fuelIcon, price: 24.99, stock: 5 },
+]
