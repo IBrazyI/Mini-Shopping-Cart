68c6a3bad30c7075842c00f738761dfea1bc8cdd
Joshua Brazington <braz.josh@yahoo.co.uk>
2026-03-30 11:51:41 +0100

    implement shopItem logic and ensure the storefront passes the correct data

--- my-app/src/components/shopItem/shopItem.tsx
+++ my-app/src/components/shopItem/shopItem.tsx
@@ -1,17 +1,55 @@
 import './shopItem.css'
+import { useState } from 'react'
 
-function ShopItem() {
+type Item = {
+    id: number;
+    name: string;
+    image: string;
+    price: number,
+    stock: number
+}
+
+type Props = {
+    item: Item
+}
+
+function ShopItem({ item }: Props) {
+    const [quantity, setQuantity] = useState(0)
+    const isAddDisabled = quantity <= 0
+
+    const increment = () =>
+        setQuantity((currentQuantity) =>
+            Math.min(item.stock, currentQuantity + 1)
+        )
+    const decrement = () => setQuantity((currentQuantity) =>
+        Math.max(0, currentQuantity - 1)
+    )
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = Math.max(0, Number(event.target.value) || 0)
+        setQuantity(value)
+    }
     return (
-        <div>
-            <h2>Shop Item Name</h2>
-            <p>Image</p>
-            <p>Price</p>
-            <p>Left in stock</p>
+        <div className="shop-item">
+            <h2>{item.name}</h2>
+            <img src={item.image} alt={item.name} />
+            <p>Price: £{item.price}</p>
+            <p>Left in stock: {item.stock}</p>
+
             <div>
-                <button>Less</button>
-                <input type='number'/>
-                <button>More</button>
+                <button type="button" onClick={decrement}>Less</button>
+                <input
+                    className="item-input"
+                    type="number"
+                    value={quantity}
+                    min={0}
+                    max={item.stock}
+                    onChange={handleChange}
+                    readOnly
+                />
+                <button type="button" onClick={increment}>More</button>
             </div>
+
+            <button type='button' disabled={isAddDisabled}>Add to basket</button>
         </div>
     )
 }
--- my-app/src/components/storefront/storefont.tsx
+++ my-app/src/components/storefront/storefont.tsx
@@ -1,15 +1,21 @@
 import './storefront.css'
 import ShopItem from '../shopItem/shopItem.tsx'
-import {items} from '../../data/shopItems.ts'
+import { items } from '../../data/shopItems.ts'
 
 function Storefront() {
     return (
         <>
             <div>
                 <h1>Doomsday Supply Shop</h1>
+                <button type="button">Basket</button>
             </div>
-            <div className='storefront'>
-                <ShopItem></ShopItem>
+            <div className="storefront">
+                {items.map(item => (
+                    <ShopItem
+                        key={item.id}
+                        item={item}
+                        />
+                ))}
             </div>
         </>
     )
